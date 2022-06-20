778c2e1974778d68972008d46684d7794c5ffcfd
Rodrigo Miranda <63809640+LaloMiranda@users.noreply.github.com>
2022-06-20 16:35:25 -0300

    feat: Finalizado el proceso instalación Elastic

--- script.sh
+++ script.sh
@@ -1,28 +1,32 @@
 #!/bin/bash
 
-echo "Paso 1: Instalar Elasticsearch y Kibana"
+CurrentPath=$PWD
+
+echo "Paso 1: Instalar Elasticsearch"
 
 curl -fsSL https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
 
 echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-7.x.list
 
 sudo apt update
-sudo apt install elasticsearch kibana
+sudo apt install elasticsearch
 
+ip -brief address show
 ip -brief address show > ip.txt
-IP=$(awk '/eth1/ {print $3}' ip.txt)
+Line=$(awk '/enp0s3/ {print $3}' ip.txt)
+IP=${Line::-3}
 echo My ip address: $IP 
 
 echo "Paso 2: Configuración de Elasticsearch"
 
 echo "Configuración de redes de Elasticsearch"
-sed -i "s|network.host: 192.168.0.1|network.bind_host: ["127.0.0.1", ""$IP""]|g" /etc/elasticsearch/elasticsearch.yml
+sed -i "s|network.host: 192.168.0.1|network.bind_host: [\"127.0.0.1\", \""$IP"\"]|g" /etc/elasticsearch/elasticsearch.yml
 
 sed -i '$a discovery.type: single-node' /etc/elasticsearch/elasticsearch.yml
 sed -i '$a xpack.security.enabled: true' /etc/elasticsearch/elasticsearch.yml
 
-sudo ufw allow in on eth1
-sudo ufw allow out on eth1
+sudo ufw allow in on enp0s3
+sudo ufw allow out on enp0s3
 
 echo "Iniciando Elasticsearch"
 sudo systemctl start elasticsearch.service
@@ -30,15 +34,15 @@ sudo systemctl start elasticsearch.service
 echo "Configuración de contraseñas de Elasticsearch"
 
 cd /usr/share/elasticsearch/bin
-sudo ./elasticsearch-setup-passwords auto > passwords.txt
+sudo yes | sudo ./elasticsearch-setup-passwords auto > $CurrentPath/passwords.txt
 
 
-echo "Paso 3: Configuración de Kibana"
+# echo "Paso 3: Configuración de Kibana"
 
-echo "Habilitación xpack.securityen Kibana"
-cd /usr/share/kibana/bin/
-KIBANA=$(sudo ./kibana-encryption-keys generate -q)
+# echo "Habilitación xpack.securityen Kibana"
+# cd /usr/share/kibana/bin/
+# KIBANA=$(sudo ./kibana-encryption-keys generate -q)
 
-sed -i "$a "$KIBANA"" /etc/kibana/kibana.yml
+# sed -i "$a "$KIBANA"" /etc/kibana/kibana.yml
 
-
+# sed -i "s|#server.host: \"localhost\"|server.host: \""$IP"\"|g" /etc/kibana/kibana.yml
